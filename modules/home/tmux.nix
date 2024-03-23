f736b309ee54e0835a154c3ce71962c8c201a77b
AsgrimS <jan.semik@gmail.com>
2024-03-23 16:35:53 +0100

    fix tmux colors

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -3,6 +3,10 @@
     enable = true;
     clock24 = true;
 
+    extraConfig = ''
+      set -ag terminal-overrides ',xterm-256color:RGB'
+    '';
+
     plugins = with pkgs; [
       tmuxPlugins.onedark-theme
     ];
