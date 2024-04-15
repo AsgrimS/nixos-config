bc134ab8634ffff45ab052787db96f73ae2542ca
AsgrimS <jan.semik@gmail.com>
2024-04-15 17:41:30 +0200

    sudo clean command

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -39,7 +39,7 @@
   programs.zsh.shellAliases = {
     n-rebuild = "sudo nixos-rebuild switch --flake ~/.config/nixos-config#laptop";
     n-update = "nix flake update -I ~/.config/nixos-config";
-    n-clean = "nix-collect-garbage";
+    n-clean = "sudo nix-collect-garbage -d";
     j = "z";
   };
 
