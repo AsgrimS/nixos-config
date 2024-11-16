3c005eb9cc976c4932556843641d52ff9646cd8e
AsgrimS <jan.semik@gmail.com>
2024-11-16 18:10:05 +0100

    libclang

--- modules/home/android-studio.nix
+++ modules/home/android-studio.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     android-studio
+    libclang
   ];
 
   home.sessionVariables = rec{
     ANDROID_HOME = "$HOME/Android/Sdk";
     JAVA_HOME = "${pkgs.android-studio}/jbr";
+    LIBCLANG_PATH = "${pkgs.libclang}/lib";
   };
 }
