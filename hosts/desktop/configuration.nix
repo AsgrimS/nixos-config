76c82a64009ce16bec1fd933f4cd8a23fa97133b
AsgrimS <jan.semik@gmail.com>
2024-12-16 20:18:04 +0100

    add trusted user

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -35,6 +35,10 @@
     options = "--delete-older-than +5";
   };
 
+  nix.extraOptions = ''
+    trusted-users = root asgrim
+  '';
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
